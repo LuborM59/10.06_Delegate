ed0f7c1a0570f4f9cc57cec182c006544ef5300f
Lubor Martinský <lubor.martinsky@hotmail.com>
2018-08-31 14:15:23 +0200

    push

--- 10.06_Delegate/Program.cs
+++ 10.06_Delegate/Program.cs
@@ -4,12 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _10._06_Delegate
+namespace _10_Delegate
 {
+    delegate int MyDelegate(int a, int b);
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(Sum(10, 20));
+
+            MyDelegate d = new MyDelegate(Sum);
+            Console.WriteLine(d(30, 40));
+        }
+
+        static int Sum(int a, int b)
+        {
+            return a + b;
         }
     }
 }
